07aee068d00d5f5c4803e735952de57a0bfd71f0
Adam Kotas <adamko034@outlook.com>
2018-11-08 13:02:16 +0100

    production configuration

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT || 5000);
