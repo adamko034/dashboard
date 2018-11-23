bac71f04d7f31cc3d0f6db7a90da72eef3f8e0b5
Adam Kotas <adamko034@outlook.com>
2018-11-23 22:40:28 +0100

    api: forecast

--- routes/openWeatherMapRoutes.js
+++ routes/openWeatherMapRoutes.js
@@ -1,5 +1,6 @@
 const keys = require('../config/keys');
 const axios = require('axios');
+const _ = require('lodash');
 
 const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/';
 const openWeatherApiKeyParam = `&APPID=${keys.openWeatherMapApiKey}`;
@@ -25,5 +26,25 @@ module.exports = app => {
       wind: wind.speed,
       clouds: clouds.all
     });
+  });
+
+  app.get('/api/weather/forecast/:city/:days?', async (req, res) => {
+    const url = `${openWeatherUrl}forecast?q=${
+      req.params.city
+    },pl&units=metric${openWeatherApiKeyParam}`;
+
+    const {data: {list: forecast}} = await axios.get(url);
+    const forecastMapped = _.take(forecast, (Math.round((req.params.days || 3) * 24 / 3))).map(f => {
+      return {
+        date: new Date(parseInt(f.dt) * 1000),
+        temp: f.main.temp,
+        wind: f.wind.speed,
+        clouds: f.clouds.all,
+        rain: f.rain["3h"] || 0,
+        snow: f.snow ? f.snow["3h"] || 0 : 0
+      }
+    });
+    
+    res.send(forecastMapped);
   });
 };
